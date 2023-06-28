afd5a4b061452b3e40778a3d5ba0822794cf569f
wwan13 <wwan13@naver.com>
2023-06-29 01:07:38 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/com/malgo/malgoserver/MalgoServerApplication.java
@@ -0,0 +1,13 @@
+package com.malgo.malgoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalgoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalgoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malgo/malgoserver/MalgoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malgo.malgoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalgoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
