0676e529f994d2f6a62dc2b99d056563270b1f34
jang-yujin <jyj9747@naver.com>
2023-06-29 10:57:31 +0900

    feat : Keyword entity 생성

--- src/main/java/com/malgo/malgoserver/Keyword/Keyword.java
+++ src/main/java/com/malgo/malgoserver/Keyword/Keyword.java
@@ -1,2 +1,26 @@
-package com.malgo.malgoserver.Keyword;public class Keyword {
+package com.malgo.malgoserver.Keyword;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Keyword {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "keyword_id")
+	@NotNull
+	private Long id;
+
+	@NotNull private String tag;
+
+	@Builder
+	public Keyword(String tag) {
+		this.tag = tag;
+	}
 }
