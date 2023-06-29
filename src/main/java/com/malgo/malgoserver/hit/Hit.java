a00c3c7486bfdb98fe2cc84c403e1df148e1aecf
jang-yujin <jyj9747@naver.com>
2023-06-29 10:58:52 +0900

    feat : Hit entity 생성

--- src/main/java/com/malgo/malgoserver/hit/Hit.java
+++ src/main/java/com/malgo/malgoserver/hit/Hit.java
@@ -1,2 +1,32 @@
-package com.malgo.malgoserver.hit;public class Hit {
+package com.malgo.malgoserver.hit;
+
+import com.malgo.malgoserver.Keyword.Keyword;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Hit {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "hit_id")
+	@NotNull
+	private Long id;
+
+	@NotNull private int count;
+
+	@NotNull
+	@OneToOne(fetch = FetchType.LAZY)
+	private Keyword keyword;
+
+	@Builder
+	public Hit(int count, Keyword keyword) {
+		this.count = count;
+		this.keyword = keyword;
+	}
 }
