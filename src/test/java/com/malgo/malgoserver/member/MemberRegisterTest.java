35b8bf5aafe8531e5d29b2b8bae6ecb6d471171d
jang-yujin <jyj9747@naver.com>
2023-06-29 16:26:07 +0900

    test :  Member 회원가입 (미완)

---
+++ src/test/java/com/malgo/malgoserver/member/MemberRegisterTest.java
@@ -0,0 +1,72 @@
+package com.malgo.malgoserver.member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.malgo.malgoserver.company.Company;
+import com.malgo.malgoserver.company.CompanyRepository;
+import com.malgo.malgoserver.keyword.Keyword;
+import com.malgo.malgoserver.keyword.KeywordRepository;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+@ActiveProfiles("dev")
+public class MemberRegisterTest {
+
+	@Autowired MockMvc mvc;
+
+	@Autowired MemberRepository memberRepository;
+	@Autowired CompanyRepository companyRepository;
+	@Autowired KeywordRepository keywordRepository;
+
+	@Autowired ObjectMapper mapper;
+
+	String[] keywords = {"독서", "AI", "자기계발"};
+
+	private static final String CERTIFICATION_ID = "hackathon@gmail.com";
+	private static final String PASSWORD = "hackathon1234";
+
+	private static final String COMPANY_NAME = "삼성전자";
+	private static final String COMPANY_CODE = "dEb25A";
+
+	@Test
+	public void 정상_회원가입() throws Exception {
+		Company company = Company.builder().name("삼성전자").code(COMPANY_CODE).build();
+
+		Company savedCompany = companyRepository.save(company);
+
+		assertThat(savedCompany).isNotNull();
+		assertThat(savedCompany.getName()).isEqualTo(COMPANY_NAME);
+		assertThat(savedCompany.getCode()).isEqualTo(COMPANY_CODE);
+		assertThat(savedCompany.getCreateAt()).isNotNull();
+		assertThat(savedCompany.getUpdateAt()).isNull();
+
+		List<Keyword> keywordList = new ArrayList<>();
+		for (int i = 0; i < keywords.length; i++) {
+			keywordList.add(Keyword.builder().tag("독서").build());
+		}
+	}
+
+	@Test
+	public void 회사_생성() {
+		Company company = Company.builder().name("삼성전자").code(COMPANY_CODE).build();
+
+		Company savedCompany = companyRepository.save(company);
+
+		assertThat(savedCompany).isNotNull();
+		assertThat(savedCompany.getName()).isEqualTo(COMPANY_NAME);
+		assertThat(savedCompany.getCode()).isEqualTo(COMPANY_CODE);
+		assertThat(savedCompany.getCreateAt()).isNotNull();
+		assertThat(savedCompany.getUpdateAt()).isNull();
+	}
+}
