31f7f001445b7d842d357949996eb72cf52e1083
wwan13 <wwan13@naver.com>
2023-06-29 14:55:28 +0900

    feat : group post 엔티티, 레포지토리 구현

---
+++ src/main/java/com/malgo/malgoserver/grouppost/GroupPost.java
@@ -0,0 +1,31 @@
+package com.malgo.malgoserver.grouppost;
+
+import com.malgo.malgoserver.group.Group;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class GroupPost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "group_post_id")
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @Builder
+    public GroupPost(String content, Group group) {
+        this.content = content;
+        this.group = group;
+    }
+}
