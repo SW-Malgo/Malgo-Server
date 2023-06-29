d813f67b41d030d91be650e777c3770603b2ba5e
wwan13 <wwan13@naver.com>
2023-06-29 23:16:49 +0900

    refactor : KeywordRepository 겹침 문제 해결

--- src/main/java/com/malgo/malgoserver/keyword/KeywordRepository.java
+++ src/main/java/com/malgo/malgoserver/keyword/KeywordRepository.java
@@ -1,12 +1,9 @@
 package com.malgo.malgoserver.keyword;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
-@Repository
-public interface KeywordRepository extends JpaRepository<Keyword, Long> {
-    Optional<Keyword> findByTag(String tag);
-    Boolean existByTag(String tag);
+public interface KeywordRepository extends KeywordRepositoryCustom {
+	Optional<Keyword> findByTag(String tag);
+
+	Boolean existByTag(String tag);
 }
---
+++ src/main/java/com/malgo/malgoserver/keyword/KeywordRepositoryCustomImpl.java
@@ -0,0 +1,31 @@
+package com.malgo.malgoserver.keyword;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class KeywordRepositoryCustomImpl implements KeywordRepositoryCustom {
+	private final EntityManager em;
+
+	public Keyword save(Keyword keyword) {
+		em.persist(keyword);
+		return keyword;
+	}
+
+	public List<Keyword> findByTags(List<String> tags) {
+		return em.createQuery("select k from Keyword k where k.tag in :tags", Keyword.class)
+				.setParameter("tags", tags)
+				.getResultList();
+	}
+
+	public List<Keyword> findAll() {
+		return em.createQuery("select k from Keyword k", Keyword.class).getResultList();
+	}
+
+	public Keyword findOne(Long id) {
+		return em.find(Keyword.class, id);
+	}
+}
