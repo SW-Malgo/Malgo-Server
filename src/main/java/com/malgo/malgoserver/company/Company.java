bb3551a3588900ae8c44baad6ecc53a4422c1660
jang-yujin <jyj9747@naver.com>
2023-06-29 10:58:01 +0900

    feat : Company entity 생성

--- src/main/java/com/malgo/malgoserver/company/Company.java
+++ src/main/java/com/malgo/malgoserver/company/Company.java
@@ -1,2 +1,28 @@
-package com.malgo.malgoserver;public class Company {
+package com.malgo.malgoserver.company;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Company {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "company_id")
+	@NotNull
+	private Long id;
+
+	@NotNull private String name;
+	@NotNull private String code;
+
+	@Builder
+	public Company(String name, String code) {
+		this.name = name;
+		this.code = code;
+	}
 }
