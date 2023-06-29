ea81512aefbb87a68798c68bcc279abbb4221b20
jang-yujin <jyj9747@naver.com>
2023-06-29 18:12:11 +0900

    test : SUCCESS - Keyword 생성 test

--- src/test/java/com/malgo/malgoserver/member/MemberRegisterTest.java
+++ src/test/java/com/malgo/malgoserver/member/MemberRegisterTest.java
@@ -5,7 +5,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.malgo.malgoserver.company.Company;
 import com.malgo.malgoserver.company.CompanyRepository;
+import com.malgo.malgoserver.keyword.Keyword;
 import com.malgo.malgoserver.keyword.KeywordRepository;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -36,23 +39,16 @@ public class MemberRegisterTest {
 	private static final String COMPANY_NAME = "삼성전자";
 	private static final String COMPANY_CODE = "dEb25A";
 
-	//	@Test
-	//	public void 정상_회원가입() throws Exception {
-	//		Company company = Company.builder().name("삼성전자").code(COMPANY_CODE).build();
-	//
-	//		Company savedCompany = companyRepository.save(company);
-	//
-	//		assertThat(savedCompany).isNotNull();
-	//		assertThat(savedCompany.getName()).isEqualTo(COMPANY_NAME);
-	//		assertThat(savedCompany.getCode()).isEqualTo(COMPANY_CODE);
-	//		assertThat(savedCompany.getCreateAt()).isNotNull();
-	//		assertThat(savedCompany.getUpdateAt()).isNull();
-	//
-	//		List<Keyword> keywordList = new ArrayList<>();
-	//		for (int i = 0; i < keywords.length; i++) {
-	//			keywordList.add(Keyword.builder().tag("독서").build());
-	//		}
-	//	}
+	@Test
+	public void 정상_회원가입() throws Exception {
+		Company company = Company.builder().name("삼성전자").code(COMPANY_CODE).build();
+		Company savedCompany = companyRepository.save(company);
+
+		List<Keyword> keywordList = new ArrayList<>();
+		for (int i = 0; i < keywords.length; i++) {
+			keywordList.add(Keyword.builder().tag(keywords[i]).build());
+		}
+	}
 
 	@Test
 	public void 회사_생성() {
@@ -65,5 +61,17 @@ public class MemberRegisterTest {
 		assertThat(savedCompany.getCode()).isEqualTo(COMPANY_CODE);
 		assertThat(savedCompany.getCreateAt()).isNotNull();
 		assertThat(savedCompany.getUpdateAt()).isNotNull();
+	}
+
+	@Test
+	public void 키워드_생성() {
+		List<Keyword> savedKeywordList = new ArrayList<>();
+		for (int i = 0; i < keywords.length; i++) {
+			savedKeywordList.add(Keyword.builder().tag(keywords[i]).build());
+		}
+
+		assertThat(savedKeywordList.size()).isEqualTo(keywords.length);
+		for (int i = 0; i < keywords.length; i++)
+			assertThat(savedKeywordList.get(i).getTag()).isEqualTo(keywords[i]);
 	}
 }
