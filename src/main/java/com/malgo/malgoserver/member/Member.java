b2ace904cd3f02cade3f24fbc8c0d9ec92810c08
jang-yujin <jyj9747@naver.com>
2023-06-29 10:58:25 +0900

    feat : Member entity 생성

--- src/main/java/com/malgo/malgoserver/member/Member.java
+++ src/main/java/com/malgo/malgoserver/member/Member.java
@@ -1,2 +1,42 @@
-package com.malgo.malgoserver.member;public class Member {
+package com.malgo.malgoserver.member;
+
+import com.malgo.malgoserver.company.Company;
+import com.malgo.malgoserver.converter.KeywordToArray;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "member_id")
+	@NotNull
+	private Long id;
+
+	@NotNull private String certificationId;
+	@NotNull private String password;
+
+	@NotNull
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Company company;
+
+	@NotNull
+	@Convert(converter = KeywordToArray.class)
+	private List<Long> keyword = new ArrayList<>();
+
+	@Builder
+	public Member(String certificationId, String password, Company company) {
+		this.certificationId = certificationId;
+		this.password = password;
+		this.company = company;
+	}
 }
