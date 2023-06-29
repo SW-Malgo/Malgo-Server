cd5f1203481a76b883c0ff947e9fda3129a2aefb
jang-yujin <jyj9747@naver.com>
2023-06-29 15:35:39 +0900

    feat : Company builder 적용

--- src/main/java/com/malgo/malgoserver/company/Company.java
+++ src/main/java/com/malgo/malgoserver/company/Company.java
@@ -1,17 +1,15 @@
 package com.malgo.malgoserver.company;
 
+import java.time.LocalDateTime;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Getter
-@Builder
+@NoArgsConstructor
 public class Company {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,14 +20,12 @@ public class Company {
 	@NotNull private String name;
 	@NotNull private String code;
 
-	@NotNull
-	@CreationTimestamp
-	private LocalDateTime createAt;
-	@NotNull
-	@UpdateTimestamp
-	private LocalDateTime updateAt;
+	@NotNull @CreationTimestamp private LocalDateTime createAt;
+	@NotNull @UpdateTimestamp private LocalDateTime updateAt;
 
-	public Company() {
-
+	@Builder
+	public Company(String name, String code) {
+		this.name = name;
+		this.code = code;
 	}
 }
